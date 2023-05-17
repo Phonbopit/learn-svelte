a43943baaa5b09eafed901dff80d0a885c6085a9
Chai Phonbopit <phonbopit@gmail.com>
2023-05-17 22:03:26 +0700

    finish introduction, reactivity and props

---
+++ src/routes/tutorials/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import HelloWorld from './01-HelloWorld.svelte'
+	import Reactive from './02-Reactive.svelte'
+</script>
+
+<h1>Learn from tutorials</h1>
+
+<h3>01 - Hello World</h3>
+<HelloWorld />
+
+<h3>02 - Reactive</h3>
+<Reactive todos={['default todo', 'todo2']} />
---
+++ src/routes/tutorials/02-Reactive.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	let count = 0
+	let newTodo = ''
+
+	export let todos: string[] = []
+
+	function handleClick() {
+		count += 1
+	}
+
+	const addTodo = (newTodo: string) => {
+		todos = [...todos, newTodo]
+	}
+
+	// watch count and update doubled
+	// declare variable with $: prefix
+	$: doubled = count * 2
+
+	// run a statement
+	$: console.log('count is', count)
+
+	// run a group statements
+	$: {
+		console.log('group: count is', count)
+		console.log('group: doubled is', doubled)
+	}
+</script>
+
+<button on:click={handleClick}>Click me!</button>
+
+<p>Total count is {count}</p>
+<p>Total doubled is {doubled}</p>
+
+<h4>List of Todos</h4>
+{#each todos as todo}
+	<p>{todo}</p>
+{/each}
+
+<input type="text" bind:value={newTodo} />
+<button on:click={() => addTodo(newTodo)}>Add Todo</button>
