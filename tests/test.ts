88a97afe6a45a64c796147d32349c5f42cf7aa88
Chai Phonbopit <phonbopit@gmail.com>
2023-05-27 00:29:48 +0700

    testing - add playwright script for tutorials page

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,24 @@ test('index page has expected h1', async ({ page }) => {
   await page.goto('/')
   await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible()
 })
+
+test.describe('Tutorials Page', () => {
+  test.describe('01-HelloWorld', () => {
+    test('h1 has expected text', async ({ page }) => {
+      await page.goto('/tutorials')
+      await expect(page.getByRole('heading', { name: 'Devahoy' })).toBeVisible()
+    })
+  })
+
+  test.describe('02-Reactive', () => {
+    test('should increase count by 1 when button is clicked.', async ({ page }) => {
+      await page.goto('/tutorials')
+
+      const button = await page.getByTestId('click-me')
+
+      await button.click()
+
+      await expect(page.getByText('Total count is 1')).toBeVisible()
+    })
+  })
+})
