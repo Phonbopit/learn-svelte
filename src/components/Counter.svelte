67b2d30db999fd4c81b99405e7ade0d4c80b2d59
Chai Phonbopit <phonbopit@gmail.com>
2023-05-26 22:50:58 +0700

    add simple counter test

---
+++ src/components/Counter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let count: number = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<button on:click={handleClick}>Click me!</button>
+
+<p data-testid="count">Total clicks is {count}</p>
